5e8767dcef4ccd775fed32019a091440b83bfd34
hephaestus <hephaestus@zoomtop>
2019-06-01 21:17:13 -0400

    rounded cyl

--- RoundedCylinder.groovy
+++ RoundedCylinder.groovy
@@ -1,7 +1,7 @@
 //create a rounded cube
-import eu.mihosoft.vrl.v3d.RoundedCylinder;
 
-CSG roundedCylinder = new RoundedCylinder(5.0,40.0)
+
+CSG roundedCylinder = new eu.mihosoft.vrl.v3d.RoundedCylinder(5.0,40.0)
 				.cornerRadius(3)// sets the radius of the corner
 				.toCSG()// converts it to a CSG tor display
 return roundedCylinder
