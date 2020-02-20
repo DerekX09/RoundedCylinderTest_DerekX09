e40d0dd3379ca43516094d0b496c1e4626b3c714
Kevin Harrington <mad.hephaestus@gmail.com>
2020-02-20 16:59:53 -0500

    bigger rounder cylindar

--- RoundedCylinderTest.groovy
+++ RoundedCylinderTest.groovy
@@ -1,6 +1,6 @@
 
-CSG roundedCylinder = new RoundedCylinder(5.0,40.0)
-                                .cornerRadius(3)// sets the radius of the corner
+CSG roundedCylinder = new RoundedCylinder(50,60.0)
+                                .cornerRadius(10)// sets the radius of the corner
                                 .toCSG()// converts it to a CSG tor display
 
 return roundedCylinder
